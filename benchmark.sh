9e5959769884b20bcf8da526731c4d33152c8b0b
Adam Ivora <xivora@fi.muni.cz>
2021-09-15 08:14:22 +0200

    Update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -8,6 +8,7 @@ OUTPUT_PATH=$(realpath benchmark)
 PYTHON=$(realpath $VENV_PATH/bin/python)
 TRAITS=$(for TRAIT in $TRAITS_PATH/*[0-9][0-9]; do echo ${TRAIT#$TRAITS_PATH/}; done)
 SAMPLING_SECONDS=600
+SAMPLING_RATE=1000
 
 mkdir -p $OUTPUT_PATH
 
@@ -15,6 +16,6 @@ cd $TRAITS_PATH
 for TRAIT in $TRAITS
 do
     echo "Running trait $TRAIT..."
-    py-spy record -f speedscope -s -o $OUTPUT_PATH/$TRAIT.json -d $SAMPLING_SECONDS -- $PYTHON run_traits_single.py -c all -a any -n $TRAIT
+    py-spy record -f speedscope -s -o $OUTPUT_PATH/$TRAIT.json -d $SAMPLING_SECONDS -r $SAMPLING_RATE -- $PYTHON run_traits_single.py -c all -a any -n $TRAIT
     ps -f | grep "run_traits_single.py" | awk '{print $2}' | xargs kill 2>/dev/null || true
 done
